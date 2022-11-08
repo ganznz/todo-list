9e65017d2e5bf148ff79a4b1560a9004c25cd62b
Ganz <loganbellinghamlb@gmail.com>
2022-11-09 10:59:11 +1300

    Most task settings sidebar values are linked to the task object

--- src/modules/renderDOM.js
+++ src/modules/renderDOM.js
@@ -52,16 +52,23 @@ export default class renderDOM {
     document.querySelectorAll('.tasks-container').forEach(container => container.innerHTML = "");
   }
 
-  static toggleTaskSettingsView = (task = null) => {
+  static showTaskSettingsView = (task) => {
     const sidebarBlur = document.querySelector('.sidebar-blur');
-    sidebarBlur.classList.toggle('visible');
+    sidebarBlur.classList.add('visible');
     const taskSettingsDOM = document.querySelector('.task-settings');
-    taskSettingsDOM.classList.toggle('visible');
+    taskSettingsDOM.classList.add('visible');
     const titleInput = taskSettingsDOM.querySelector('.task-minor-info-section > .sidebar-title > input');
-    titleInput.setAttribute('value', task.name)
+    titleInput.setAttribute('value', task.name);
+    titleInput.value = task.name;
     const taskStatusLabel = taskSettingsDOM.querySelector('.task-minor-container > .task-status > h5');
     taskStatusLabel.className = '';
     taskStatusLabel.classList.add(task.status);
     taskStatusLabel.textContent = task.status == 'upcoming' ? 'Upcoming' : (task.status == "inprogress" ? 'In Progress' : 'Completed');
+    const taskPriorityLabel = taskSettingsDOM.querySelector('.task-minor-container > .task-priority > h5');
+    taskPriorityLabel.className = '';
+    taskPriorityLabel.classList.add(task.priority);
+    taskPriorityLabel.textContent = task.priority == 'low' ? 'Low' : (task.priority == "medium" ? 'Medium' : 'High');
+    const taskDescription = taskSettingsDOM.querySelector('.task-main-info-section > .task-description > textarea');
+    taskDescription.setAttribute('value', task.description);
   }
 }
