66ba8dd8705542bb2035f3d3cdf2fc9e428ca86a
Ganz <loganbellinghamlb@gmail.com>
2022-11-10 16:45:50 +1300

    Implementing foundation of task updating

--- src/modules/task.js
+++ src/modules/task.js
@@ -2,15 +2,17 @@ export default class Task {
   #name;
   #status;
   #priority;
+  #index
   #dateCreated;
   #dateDue;
   #description = "Add some information to this task...";
   #todos = [];
   
-  constructor(name, status, priority) {
+  constructor(name, status, priority, index) {
     this.#name = name;
     this.#status = status.toLowerCase();
     this.#priority = priority.toLowerCase();
+    this.#index = index;
     this.#dateCreated; // placeholder
     this.#dateDue; // set for same day task was created
   }
@@ -18,12 +20,13 @@ export default class Task {
   get name() { return this.#name };
   get status() { return this.#status };
   get priority() { return this.#priority };
+  get index() { return this.#index };
   get dateCreated() { return this.#dateCreated };
   get dueDate() { return this.#dateDue };
   get description() { return this.#description };
   
   set name(value) {
-    
+
   };
 
   set status(value) {
--- src/modules/tasksFolder.js
+++ src/modules/tasksFolder.js
@@ -1,6 +1,8 @@
 import renderDOM from './renderDOM';
 import Task from './task';
 
+export let allFolders = []; // instances of the Folder class get pushed into this
+
 export default class Folder {
   #name = 'Folder #num';
   #tasks = [];
@@ -13,7 +15,7 @@ export default class Folder {
       this.createTask('inprogress');
       this.createTask('completed');
     }
-  }
+  };
 
   get name() { return this.#name };
   get tasks() { return this.#tasks };
@@ -21,20 +23,20 @@ export default class Folder {
 
   set name(value) {
     
-  }
+  };
 
   createTask = (e) => {
     this.#numOfTasks++;
 
     const task = (e == 'upcoming' || e == 'inprogress' || e == 'completed')
-    ? new Task(`Task ${this.numOfTasks}`, e, 'low')
-    : new Task(`Task ${this.numOfTasks}`, this.determineTaskStatus(e), 'low');
+    ? new Task(`Task ${this.numOfTasks}`, e, 'low', this.numOfTasks - 1)
+    : new Task(`Task ${this.numOfTasks}`, this.determineTaskStatus(e), 'low', this.numOfTasks - 1);
     
     this.#tasks.push(task);
-    const taskElements = renderDOM.createTaskElements(task);
+    const taskElements = renderDOM.createTaskElements(task, this.numOfTasks - 1);
     taskElements.addEventListener('click', () => renderDOM.showTaskSettingsView(task));
     renderDOM.appendTaskElementsToDOM(task, taskElements);
-  }
+  };
 
   determineTaskStatus = e => {
     if (e.target.classList.contains('upcoming-tasks')) {
@@ -44,9 +46,23 @@ export default class Folder {
     } else {
       return 'completed';
     }
-  }
+  };
+
+  updateTask = index => {
+    let taskObj = this.tasks[index];
+    // let taskElementsDOM = document.getElementById(index);
+    taskObj.name = document.querySelector('.sidebar-title > input').value;
+    taskObj.status = document.querySelector('.task-minor-container > .task-status > h5').className;
+    taskObj.priority = document.querySelector('.task-minor-container > .task-priority > h5').className;
+  };
+
+  // updateTaskIndexes = () => {
+  //   let index = 0
+  //   document.querySelectorAll('.task-perspective-container').forEach(element => element.setAttribute('id', index));
+  //   index++
+  // };
 
   deleteTask = () => {
     this.#numOfTasks--;
-  }
+  };
 }
