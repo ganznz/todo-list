267a02aa74d85df51d7fe69a890e457ef5f3dc99
Ganz <loganbellinghamlb@gmail.com>
2022-11-10 16:29:37 +1300

    Minor changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import Folder from './modules/tasksFolder';
+import Folder, { allFolders } from './modules/tasksFolder';
 import Task from './modules/task';
 import renderDOM from './modules/renderDOM';
 import { iterateEventOverNodeList } from './modules/helperFunctions';
@@ -7,17 +7,15 @@ import { iterateEventOverNodeList } from './modules/helperFunctions';
 
 const myFolder = new Folder();
 
+// add tasks
 const addTaskButtons = document.querySelectorAll('.add-tasks');
 iterateEventOverNodeList(addTaskButtons, 'click', e => {
   myFolder.createTask(e);
+  // myFolder.updateTaskIndexes();
 });
 
 // close sidemenus
-const sidebarBlur = document.querySelector('.sidebar-blur');
 const taskSettingsDOM = document.querySelector('.task-settings');
 document.addEventListener('click', e => {
-  if (e.target.classList.contains('task-settings-exit-btn') || (e.target.classList.contains('sidebar-blur'))) {
-    [taskSettingsDOM, sidebarBlur].forEach(element => element.classList.remove('visible'));
-    setTimeout(() => { [taskSettingsDOM, sidebarBlur].forEach(element => element.setAttribute('style', 'display: none')) }, 1000);
-  }
+  renderDOM.closeTaskSettingsView(e);
 });
