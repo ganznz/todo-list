5e9a03bf3ab185251e8195ef71e7cd187ee22477
Ganz <loganbellinghamlb@gmail.com>
2022-11-13 12:17:52 +1300

    Minor changes

--- src/modules/helperFunctions.js
+++ src/modules/helperFunctions.js
@@ -3,3 +3,17 @@ export const iterateEventOverNodeList = (list, event, fn) => {
     list[i].addEventListener(event, fn);
   }
 }
+
+const txInitialHeight = 50;
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  (tx[i].value == 'Add a description for this task!') ? tx[i].setAttribute("style", `height: ${txInitialHeight}px; overflow-y:hidden`)
+  : tx[i].setAttribute("style", `height: ${(tx[i].scrollHeight)}px; overflow-y:hidden;`)
+  
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
