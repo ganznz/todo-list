0d831445ec0ed31503a26d4f2ced4c8bbf1444b7
Ganz <loganbellinghamlb@gmail.com>
2022-11-13 12:18:03 +1300

    Minor changes

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import Task from './modules/task';
 import renderDOM from './modules/renderDOM';
 import { iterateEventOverNodeList } from './modules/helperFunctions';
 
-
-const myFolder = new Folder();
+// default generated folders
+for (let i = 0; i < 4; i++) {
+  renderDOM.clearFolderTasksElements(); // clears old folder viewport before showing new one
+  const folderObj = new Folder();
+  allFolders.push(folderObj);
+  renderDOM.addFolderToSidebar(folderObj);
+}
 
 // add tasks
 const addTaskButtons = document.querySelectorAll('.add-tasks');
 iterateEventOverNodeList(addTaskButtons, 'click', e => {
-  myFolder.createTask(e);
-  // myFolder.updateTaskIndexes();
-});
-
-// close sidemenus
-const taskSettingsDOM = document.querySelector('.task-settings');
-document.addEventListener('click', e => {
-  renderDOM.closeTaskSettingsView(e);
+  const selectedFolder = document.querySelector('.selected-folder');
+  const selectedFolderIndex = selectedFolder.getAttribute('folderindex');
+  allFolders[selectedFolderIndex].createTask(e);
 });
