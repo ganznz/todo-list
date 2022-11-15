2b16d1c0dc63784793b22662ad1eab2501e7ff85
Ganz <loganbellinghamlb@gmail.com>
2022-11-16 10:24:14 +1300

    Defined function that allows setting multiple element attributes in one line

--- src/modules/helperFunctions.js
+++ src/modules/helperFunctions.js
@@ -4,10 +4,17 @@ export const iterateEventOverNodeList = (list, event, fn) => {
   }
 }
 
+export const setAttributes = (element, attrs) => {
+  for (const key in attrs) {
+    element.setAttribute(key, attrs[key]);
+  }
+}
+
 const txInitialHeight = 50;
 const tx = document.getElementsByTagName("textarea");
 for (let i = 0; i < tx.length; i++) {
-  (tx[i].value == 'Add a description for this task!') ? tx[i].setAttribute("style", `height: ${txInitialHeight}px; overflow-y:hidden`)
+  (tx[i].value == 'Add a description for this task!')
+  ? tx[i].setAttribute("style", `height: ${txInitialHeight}px; overflow-y:hidden`)
   : tx[i].setAttribute("style", `height: ${(tx[i].scrollHeight)}px; overflow-y:hidden;`)
   
   tx[i].addEventListener("input", OnInput, false);
